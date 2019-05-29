2bf98695c03fa24b320325f34f0cc58c6759c3c8
tiffanycabernathy96 <tiffanycabernathy96@gmail.com>
2019-05-29 16:28:41 -0400

    Implementation of Linked List

--- DataStructures/LinkedList/LinkedList.h
+++ DataStructures/LinkedList/LinkedList.h
@@ -1,1 +1,31 @@
 #pragma once
+struct node
+{
+public:
+	int data;
+	node* next;
+	node* prev;
+};
+class LinkedList
+{
+public:
+	
+	LinkedList();
+	~LinkedList();
+
+	void addHead(int);
+	void addTail(int);
+	void addAt(int, int);
+	void deleteHead();
+	void deleteTail();
+	void deleteAt(int);
+
+	int getCount();
+
+	void display();
+
+private:
+	node* head;
+	node* tail;
+	int numElems;
+};
